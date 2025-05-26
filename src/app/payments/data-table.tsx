043528b41011a454f206d98b2975c169bfbe972c
Riya <riya.dev11042001@gmail.com>
2025-05-26 11:16:09 +0530

    spacing

--- src/app/payments/data-table.tsx
+++ src/app/payments/data-table.tsx
@@ -143,7 +143,7 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-end space-x-2 py-4 px-5">
         <Button
           variant="outline"
           size="sm"
